e41a120d8ae9f8e3b12374c565c5eccca158feb7
Manuela Onofre <102526860+onofremanuela-foodtosave@users.noreply.github.com>
2024-07-05 17:18:22 -0300

    feat: add style to home page

--- lib/screens/HomeScreen/home_screen.dart
+++ lib/screens/HomeScreen/home_screen.dart
@@ -20,46 +20,46 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(16),
         child: Stack(
           children: [
             if (isLoading) ...[
-              CircularProgressIndicator(),
+              const CircularProgressIndicator(),
             ],
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  color: Colors.blue,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: InkWell(
-                      splashColor: Colors.white,
-                      onTap: () async {
-                        String locationsJson = await rootBundle
-                            .loadString('assets/jaguar/locations.json');
-                        List<dynamic> jsonDataLocation =
-                            jsonDecode(locationsJson);
-                        List<Location> treeNodesLocations = jsonDataLocation
-                            .map((json) => Location.fromJson(json))
-                            .toList();
+                InkWell(
+                  splashColor: Colors.white,
+                  onTap: () async {
+                    String locationsJson = await rootBundle
+                        .loadString('assets/jaguar/locations.json');
+                    List<dynamic> jsonDataLocation = jsonDecode(locationsJson);
+                    List<Location> treeNodesLocations = jsonDataLocation
+                        .map((json) => Location.fromJson(json))
+                        .toList();
 
-                        String assetsJson = await rootBundle
-                            .loadString('assets/jaguar/assets.json');
-                        List<dynamic> jsonDataAssets = jsonDecode(assetsJson);
-                        List<Asset> treeNodesAssets = jsonDataAssets
-                            .map((json) => Asset.fromJson(json))
-                            .toList();
+                    String assetsJson = await rootBundle
+                        .loadString('assets/jaguar/assets.json');
+                    List<dynamic> jsonDataAssets = jsonDecode(assetsJson);
+                    List<Asset> treeNodesAssets = jsonDataAssets
+                        .map((json) => Asset.fromJson(json))
+                        .toList();
 
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => AssetScreen(
-                              locations: treeNodesLocations,
-                              assets: treeNodesAssets,
-                            ),
-                          ),
-                        );
-                      },
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => AssetScreen(
+                          locations: treeNodesLocations,
+                          assets: treeNodesAssets,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    height: 50,
+                    color: Colors.blue,
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
                       child: Row(
                         children: [
                           Text(
@@ -73,97 +73,105 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   ),
                 ),
-                Container(
-                    color: Colors.blue,
-                    child: InkWell(
-                      onTap: () async {
-                        String locationsJson = await rootBundle
-                            .loadString('assets/tobias/locations.json');
-                        List<dynamic> jsonDataLocation =
-                            jsonDecode(locationsJson);
-                        List<Location> treeNodesLocations = jsonDataLocation
-                            .map((json) => Location.fromJson(json))
-                            .toList();
+                const SizedBox(
+                  height: 16,
+                ),
+                InkWell(
+                  onTap: () async {
+                    String locationsJson = await rootBundle
+                        .loadString('assets/tobias/locations.json');
+                    List<dynamic> jsonDataLocation = jsonDecode(locationsJson);
+                    List<Location> treeNodesLocations = jsonDataLocation
+                        .map((json) => Location.fromJson(json))
+                        .toList();
 
-                        String assetsJson = await rootBundle
-                            .loadString('assets/tobias/assets.json');
-                        List<dynamic> jsonDataAssets = jsonDecode(assetsJson);
-                        List<Asset> treeNodesAssets = jsonDataAssets
-                            .map((json) => Asset.fromJson(json))
-                            .toList();
+                    String assetsJson = await rootBundle
+                        .loadString('assets/tobias/assets.json');
+                    List<dynamic> jsonDataAssets = jsonDecode(assetsJson);
+                    List<Asset> treeNodesAssets = jsonDataAssets
+                        .map((json) => Asset.fromJson(json))
+                        .toList();
 
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => AssetScreen(
-                              locations: treeNodesLocations,
-                              assets: treeNodesAssets,
-                            ),
-                          ),
-                        );
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: [
-                            Text(
-                              'Tobias Unit',
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => AssetScreen(
+                          locations: treeNodesLocations,
+                          assets: treeNodesAssets,
                         ),
                       ),
-                    )),
-                Container(
+                    );
+                  },
+                  child: Container(
+                    height: 50,
                     color: Colors.blue,
-                    child: InkWell(
-                      onTap: () async {
-                        setState(() {
-                          isLoading = true;
-                        });
-                        String locationsJson = await rootBundle
-                            .loadString('assets/apex/locations.json');
-                        List<dynamic> jsonDataLocation =
-                            jsonDecode(locationsJson);
-                        List<Location> treeNodesLocations = jsonDataLocation
-                            .map((json) => Location.fromJson(json))
-                            .toList();
-
-                        String assetsJson = await rootBundle
-                            .loadString('assets/apex/assets.json');
-                        List<dynamic> jsonDataAssets = jsonDecode(assetsJson);
-                        List<Asset> treeNodesAssets = jsonDataAssets
-                            .map((json) => Asset.fromJson(json))
-                            .toList();
-
-                        setState(() {
-                          isLoading = false;
-                        });
-
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => AssetScreen(
-                              locations: treeNodesLocations,
-                              assets: treeNodesAssets,
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Tobias Unit',
+                            style: TextStyle(
+                              color: Colors.white,
                             ),
                           ),
-                        );
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: [
-                            Text(
-                              'Apex Unit',
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                InkWell(
+                  onTap: () async {
+                    setState(() {
+                      isLoading = true;
+                    });
+                    String locationsJson = await rootBundle
+                        .loadString('assets/apex/locations.json');
+                    List<dynamic> jsonDataLocation = jsonDecode(locationsJson);
+                    List<Location> treeNodesLocations = jsonDataLocation
+                        .map((json) => Location.fromJson(json))
+                        .toList();
+
+                    String assetsJson =
+                        await rootBundle.loadString('assets/apex/assets.json');
+                    List<dynamic> jsonDataAssets = jsonDecode(assetsJson);
+                    List<Asset> treeNodesAssets = jsonDataAssets
+                        .map((json) => Asset.fromJson(json))
+                        .toList();
+
+                    setState(() {
+                      isLoading = false;
+                    });
+
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => AssetScreen(
+                          locations: treeNodesLocations,
+                          assets: treeNodesAssets,
                         ),
                       ),
-                    )),
+                    );
+                  },
+                  child: Container(
+                    height: 50,
+                    color: Colors.blue,
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Apex Unit',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ],
